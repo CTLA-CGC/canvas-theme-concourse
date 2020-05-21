d557ed94787571b34e8ad88095f2365b79f1ad4d
CTLA@CGC <61021170+CTLA-CGC@users.noreply.github.com>
2020-05-21 11:28:33 -0700

    Update Titlechange.js

--- Titlechange.js
+++ Titlechange.js
@@ -1,3 +1,5 @@
+//Found this from the Canvas Community. It is a simple script to change the title of the navigation menu. The other problem that we have is on the current syllabus tool there is a title that is called syllabus and mention of syllabus in the descriptive body text. I have limited knowledge with JavaScript to know how to change just those items when the script is at the theme level. I would need to call the page and only the text on that page replace "syallbus" with "course schedule." //
+
 $(document.body).find('a.syllabus').each(function() { //search with links only
     var tmp = $(this).children().remove();
     var text = $(this).text();
